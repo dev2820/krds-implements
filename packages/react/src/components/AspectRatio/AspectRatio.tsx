9804f4890ad3bfabebeec12f6098d4f77a0b358e
dev2820 <second28200@naver.com>
2024-05-18 18:37:20 +0900

    refactor: delete unused component

--- packages/react/src/components/AspectRatio/AspectRatio.tsx
+++
@@ -1,9 +0,0 @@
-import * as AspectRatioPrimitive from '@radix-ui/react-aspect-ratio';
-
-import type { ComponentProps } from 'react';
-
-export type AspectRatioProps = ComponentProps<typeof AspectRatio>;
-
-const AspectRatio = AspectRatioPrimitive.Root;
-
-export { AspectRatio };
