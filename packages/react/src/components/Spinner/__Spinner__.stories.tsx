d6da795696dc90e65870078f8e4f4cc9be6adfc3
dev2820 <second28200@naver.com>
2024-05-23 22:32:41 +0900

    refactor: remove comment

--- packages/react/src/components/Spinner/__Spinner__.stories.tsx
+++ packages/react/src/components/Spinner/__Spinner__.stories.tsx
@@ -2,12 +2,7 @@ import { Spinner, type SpinnerProps } from './index';
 
 const types = ['spin', 'progress'] as const;
 const sizes = ['lg', 'md', 'sm'] as const;
-/**
- * 아코디언은 한 페이지에서 관련 있는 여러 콘텐츠 섹션을 확인할 수 있도록 하는 컴포넌트로 콘텐츠 섹션의 헤더 목록이 수직으로 쌓여 있는 형태로 표현된다. 일반적으로 헤더 목록은 컨트롤 요소로 활용되며 사용자는 필요에 따라 헤더를 선택하여 하위 콘텐츠 섹션을 표시하거나 숨길 수 있다.
- *
- * KRDS Reference: https://uiux.egovframe.go.kr/guide/component/component_04_07.html<br/>
- * API Reference: https://www.radix-ui.com/primitives/docs/components/accordion
- */
+
 export default {
   title: 'Components/Spinner',
   component: Spinner,
