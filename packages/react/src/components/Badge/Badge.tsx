e90f8d129405aafaba5d25843b42f7df95a81ed6
dev2820 <second28200@naver.com>
2024-05-31 21:21:51 +0900

    refactor: Badge 컴포넌트 테스트 작성

--- packages/react/src/components/Badge/Badge.tsx
+++ packages/react/src/components/Badge/Badge.tsx
@@ -3,20 +3,17 @@ import { cva } from 'class-variance-authority';
 import { cn } from '../../utils';
 import { labelVariants } from '../Typo';
 
-const dotBadgeVariants = cva(
-  'rounded-full size-1.5',
-  {
-    variants: {
-      color: {
-        primary: 'bg-primary',
-        new: 'bg-point'
-      }
+const dotBadgeVariants = cva('rounded-full size-1.5', {
+  variants: {
+    color: {
+      primary: 'bg-primary',
+      new: 'bg-point',
     },
-    defaultVariants: {
-      color: 'primary'
-    }
-  }
-);
+  },
+  defaultVariants: {
+    color: 'primary',
+  },
+});
 
 const countBadgeVariants = cva(
   ['h-5 px-3 rounded-full', labelVariants({ size: 'sm' })],
@@ -24,29 +21,29 @@ const countBadgeVariants = cva(
     variants: {
       color: {
         primary: 'bg-primary color-primary-foreground',
-        new: 'bg-point color-point-foreground'
-      }
+        new: 'bg-point color-point-foreground',
+      },
     },
     defaultVariants: {
-      color: 'primary'
-    }
-  }
-)
+      color: 'primary',
+    },
+  },
+);
 const textBadgeVariants = cva(
-  ['rounded-2 px-3 box-border h-6',labelVariants({ size: 'sm' })],
+  ['rounded-2 px-3 box-border h-6', labelVariants({ size: 'sm' })],
   {
     variants: {
       color: {
-        primary:'',
-        secondary:'',
+        primary: '',
+        secondary: '',
         tertiary: '',
-        danger: ''
+        danger: '',
       },
       type: {
         solid: '',
         outline: '',
-        subtle:''
-      }
+        subtle: '',
+      },
     },
     defaultVariants: {
       color: 'primary',
@@ -56,92 +53,105 @@ const textBadgeVariants = cva(
       {
         color: 'primary',
         type: 'solid',
-        className: 'bg-primary color-primary-foreground'
+        className: 'bg-primary color-primary-foreground',
       },
       {
         color: 'primary',
         type: 'outline',
-        className: 'border border-primary color-primary-60'
+        className: 'border border-primary color-primary-60',
       },
       {
         color: 'primary',
         type: 'subtle',
-        className: 'bg-primary-5 color-primary-60'
+        className: 'bg-primary-5 color-primary-60',
       },
       {
         color: 'secondary',
         type: 'solid',
-        className: 'bg-secondary color-secondary-foreground'
+        className: 'bg-secondary color-secondary-foreground',
       },
       {
         color: 'secondary',
         type: 'outline',
-        className: 'border border-secondary color-secondary-60'
+        className: 'border border-secondary color-secondary-60',
       },
       {
         color: 'secondary',
         type: 'subtle',
-        className: 'bg-secondary-5 color-secondary-60'
+        className: 'bg-secondary-5 color-secondary-60',
       },
       {
         color: 'tertiary',
         type: 'solid',
-        className: 'bg-grayscale-70 color-grayscale-foreground'
+        className: 'bg-grayscale-70 color-grayscale-foreground',
       },
       {
         color: 'tertiary',
         type: 'outline',
-        className: 'border border-grayscale-70 color-grayscale-80'
+        className: 'border border-grayscale-70 color-grayscale-80',
       },
       {
         color: 'tertiary',
         type: 'subtle',
-        className: 'bg-grayscale-20 color-grayscale-80'
+        className: 'bg-grayscale-20 color-grayscale-80',
       },
       {
         color: 'danger',
         type: 'solid',
-        className: 'bg-danger color-danger-foreground'
+        className: 'bg-danger color-danger-foreground',
       },
       {
         color: 'danger',
         type: 'outline',
-        className: 'border border-danger color-danger-60'
+        className: 'border border-danger color-danger-60',
       },
       {
         color: 'danger',
         type: 'subtle',
-        className: 'bg-danger-5 color-danger-60'
+        className: 'bg-danger-5 color-danger-60',
       },
-    ]
+    ],
   },
 );
 
 export type BadgeProps = React.HTMLAttributes<HTMLSpanElement> & {
-  variant?: 'dot'|'count'|'text';
-  color?: 'primary'|'new'|'secondary'|'tertiary'|'danger';
-  type?: 'solid'|'outline'|'subtle';
+  variant?: 'dot' | 'count' | 'text';
+  color?: 'primary' | 'new' | 'secondary' | 'tertiary' | 'danger';
+  type?: 'solid' | 'outline' | 'subtle';
   count?: number;
   text?: string;
   isOverCount?: boolean;
-}
+};
 
-function Badge({ className, variant = 'text', color = 'primary', type = 'solid',count = 0,text = '', isOverCount = false, ...props }: BadgeProps) {
-  const style = variant === 'dot' 
-    ? dotBadgeVariants({ color: color!=='new'?'primary':'new' }) 
-    : variant === 'count' 
-      ? countBadgeVariants({ color: color!=='new'?'primary':'new' }) 
-      : textBadgeVariants({ color: color === 'new' ? 'danger' : color, type })
-  
+function Badge({
+  className,
+  variant = 'text',
+  color = 'primary',
+  type = 'solid',
+  count = NaN,
+  text = '',
+  isOverCount = false,
+  ...props
+}: BadgeProps) {
+  const style =
+    variant === 'dot'
+      ? dotBadgeVariants({ color: color !== 'new' ? 'primary' : 'new' })
+      : variant === 'count'
+        ? countBadgeVariants({ color: color !== 'new' ? 'primary' : 'new' })
+        : textBadgeVariants({
+            color: color === 'new' ? 'danger' : color,
+            type,
+          });
+
   return (
     <span
       role="status"
-      className={cn('inline-flex place-items-center',style, className)}
+      className={cn('inline-flex place-items-center', style, className)}
       {...props}
     >
       {variant === 'count' && `${isOverCount ? '+' : ''}${count}`}
       {variant === 'text' && text}
-      </span>
+    </span>
   );
 }
 
