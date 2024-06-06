cdc6c658d9f5bbef02730a00d943762a916cb31f
dev2820 <dev2820@users.noreply.github.com>
2024-06-06 13:20:19 +0000

    deploy: c0adb1c30e94768b956da0890ca235f4717950b0

---
+++ assets/__Link__.stories-DaeoFdPc.js
@@ -0,0 +1,16 @@
+import{j as n}from"./jsx-runtime-CexXSJP5.js";import{r as f}from"./index-BP8_t0zE.js";import{c as h}from"./cn-BeVKbU-K.js";import{E as x}from"./external-link-4l7QRTZP.js";import"./createLucideIcon-CtRCGPks.js";const s=f.forwardRef(({icon:r,className:l,children:d,...g},u)=>n.jsxs("a",{className:h("inline-flex flex-row items-center gap-1 underline color-primary-50 hover:color-primary-60 active:color-primary-70 visited:color-#5917B8",l),ref:u,...g,children:[d,r]}));s.__docgenInfo={description:"",methods:[],displayName:"Link"};const E={title:"Components/Link",component:s,parameters:{layout:"centered"},tags:["autodocs"],argTypes:{},args:{}},e={args:{href:"https://google.com"},render:r=>n.jsx(s,{...r,children:"Google"})},o={args:{href:"https://google.com",icon:n.jsx(x,{size:16})},render:r=>n.jsx(s,{...r,children:"Google"})};var t,a,i;e.parameters={...e.parameters,docs:{...(t=e.parameters)==null?void 0:t.docs,source:{originalSource:`{
+  args: {
+    href: 'https://google.com'
+  },
+  render: (props: LinkProps) => {
+    return <Link {...props}>Google</Link>;
+  }
+}`,...(i=(a=e.parameters)==null?void 0:a.docs)==null?void 0:i.source}}};var c,p,m;o.parameters={...o.parameters,docs:{...(c=o.parameters)==null?void 0:c.docs,source:{originalSource:`{
+  args: {
+    href: 'https://google.com',
+    icon: <ExternalLinkIcon size={16} />
+  },
+  render: (props: LinkProps) => {
+    return <Link {...props}>Google</Link>;
+  }
+}`,...(m=(p=o.parameters)==null?void 0:p.docs)==null?void 0:m.source}}};const G=["Default","WithIcon"];export{e as Default,o as WithIcon,G as __namedExportsOrder,E as default};
