916118130e1a0b49bb25bab90b7ce51929de7724
Terra <second28200@gmail.com>
2024-06-08 14:50:33 +0900

    feat: Modal (#30)

--- packages/react/src/components/Modal/Modal.tsx
+++ packages/react/src/components/Modal/Modal.tsx
@@ -48,16 +48,15 @@ const ModalContent = forwardRef<
     <ModalPrimitive.Content
       ref={ref}
       className={cn(
-        'fixed left-[50%] top-[50%] z-50 grid w-full max-w-lg translate-x-[-50%] translate-y-[-50%] gap-4 border bg-background p-6 shadow-lg duration-200 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[state=closed]:slide-out-to-left-1/2 data-[state=closed]:slide-out-to-top-[48%] data-[state=open]:slide-in-from-left-1/2 data-[state=open]:slide-in-from-top-[48%] sm:rounded-lg',
+        'fixed left-[50%] top-[50%] z-50 grid w-full max-w-lg translate-x-[-50%] translate-y-[-50%] gap-4 border bg-background p-6 shadow-lg duration-200',
+        'rounded-lg p-9',
+        'flex flex-col',
+        'overflow-hidden',
         className,
       )}
       {...props}
     >
       {children}
-      <ModalPrimitive.Close className="absolute right-4 top-4 rounded-sm opacity-70 ring-offset-background transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:pointer-events-none data-[state=open]:bg-accent data-[state=open]:text-muted-foreground">
-        <i className="i-t-close-big text-6" />
-        <span className="sr-only">Close</span>
-      </ModalPrimitive.Close>
     </ModalPrimitive.Content>
   </ModalPortal>
 ));
@@ -70,7 +69,7 @@ const ModalHeader = ({
 }: HTMLAttributes<HTMLDivElement>) => (
   <div
     className={cn(
-      'flex flex-col space-y-1.5 text-center sm:text-left',
+      'flex flex-col flex-none space-y-1.5 text-center mb-8',
       className,
     )}
     {...props}
@@ -83,13 +82,7 @@ const ModalFooter = ({
   className,
   ...props
 }: HTMLAttributes<HTMLDivElement>) => (
-  <div
-    className={cn(
-      'flex flex-col-reverse sm:flex-row sm:justify-end sm:space-x-2',
-      className,
-    )}
-    {...props}
-  />
+  <div className={cn('flex flex-col-reverse mt-8', className)} {...props} />
 );
 ModalFooter.displayName = 'ModalFooter';
 
@@ -116,7 +109,7 @@ const ModalDescription = forwardRef<
 >(({ className, ...props }, ref) => (
   <ModalPrimitive.Description
     ref={ref}
-    className={cn('text-sm text-muted-foreground', className)}
+    className={cn('text-sm', className)}
     {...props}
   />
 ));
--- packages/react/src/components/Modal/__Modal__.stories.tsx
+++ packages/react/src/components/Modal/__Modal__.stories.tsx
@@ -1,4 +1,7 @@
+import { XIcon } from 'lucide-react';
+
 import { Button } from '../Button';
+import { Typo } from '../Typo';
 import {
   Modal,
   ModalClose,
@@ -7,7 +10,6 @@ import {
   ModalHeader,
   ModalFooter,
   ModalTitle,
-  ModalDescription,
 } from './Modal';
 
 /**
@@ -28,37 +30,56 @@ export const Default = {
   args: {},
   render: () => {
     return (
-      <section className="p-4 w-480px">
+      <section className="p-4">
         <Modal>
           <ModalTrigger asChild>
-            <Button>Share</Button>
+            <Button>class 및 data-target 호출</Button>
           </ModalTrigger>
-          <ModalContent className="sm:max-w-md">
-            <ModalHeader>
-              <ModalTitle>Share link</ModalTitle>
-              <ModalDescription>
-                Anyone who has this link will be able to view this.
-              </ModalDescription>
-            </ModalHeader>
-            <div className="flex items-center space-x-2">
-              <div className="grid flex-1 gap-2">
-                <label htmlFor="link" className="sr-only">
-                  Link
-                </label>
-                <input
-                  id="link"
-                  defaultValue="https://ui.shadcn.com/docs/installation"
-                  readOnly
-                />
-              </div>
-              <button type="submit" className="px-3">
-                <span className="sr-only">Copy</span>
-              </button>
-            </div>
-            <ModalFooter className="sm:justify-end">
+          <ModalContent className="max-w-xl max-h-[calc(100vh_-_2rem)]">
+            <ModalHeader className="flex flex-row justify-between items-center">
+              <ModalTitle>모달 제목</ModalTitle>
               <ModalClose asChild>
-                <Button type="button" size="md">
-                  Close
+                <Button variant="text">
+                  <XIcon size={20} />
+                </Button>
+              </ModalClose>
+            </ModalHeader>
+            <div className="overflow-y-scroll flex-1">
+              <Typo.Title as="h3">내용 타이틀</Typo.Title>
+              시작 대화 상자는 사용자에게 작업에 대해 알리고 중요한 정보를
+              포함하거나 결정이 필요하거나 여러 작업을 포함할 수 있습니다. 대화
+              상자는 사용자에게 작업에 대해 알리고 중요한 정보를 포함하거나
+              결정이 필요하거나 여러 작업을 포함할 수 있습니다. 대화 상자는
+              사용자에게 작업에 대해 알리고 중요한 정보를 포함하거나 결정이
+              필요하거나 여러 작업을 포함할 수 있습니다. 대화 상자는 사용자에게
+              작업에 대해 알리고 중요한 정보를 포함하거나 결정이 필요하거나 여러
+              작업을 포함할 수 있습니다. 대화 상자는 사용자에게 작업에 대해
+              알리고 중요한 정보를 포함하거나 결정이 필요하거나 여러 작업을
+              포함할 수 있습니다. 대화 상자는 사용자에게 작업에 대해 알리고
+              중요한 정보를 포함하거나 결정이 필요하거나 여러 작업을 포함할 수
+              있습니다. 대화 상자는 사용자에게 작업에 대해 알리고 중요한 정보를
+              포함하거나 결정이 필요하거나 여러 작업을 포함할 수 있습니다. 대화
+              상자는 사용자에게 작업에 대해 알리고 중요한 정보를 포함하거나
+              결정이 필요하거나 여러 작업을 포함할 수 있습니다. 대화 상자는
+              사용자에게 작업에 대해 알리고 중요한 정보를 포함하거나 결정이
+              필요하거나 여러 작업을 포함할 수 있습니다. 대화 상자는 사용자에게
+              작업에 대해 알리고 중요한 정보를 포함하거나 결정이 필요하거나 여러
+              작업을 포함할 수 있습니다. 대화 상자는 사용자에게 작업에 대해
+              알리고 중요한 정보를 포함하거나 결정이 필요하거나 여러 작업을
+              포함할 수 있습니다. 대화 상자는 사용자에게 작업에 대해 알리고
+              중요한 정보를 포함하거나 결정이 필요하거나 여러 작업을 포함할 수
+              있습니다. 대화 상자는 사용자에게 작업에 대해 알리고 중요한 정보를
+              포함하거나 결정이 필요하거나 여러 작업을 포함할 수 있습니다. 끝
+            </div>
+            <ModalFooter className="justify-end flex flex-row gap-4">
+              <ModalClose asChild>
+                <Button variant="tertiary" type="button" size="md">
+                  아니요
+                </Button>
+              </ModalClose>
+              <ModalClose asChild>
+                <Button variant="primary" type="button" size="md">
+                  예
                 </Button>
               </ModalClose>
             </ModalFooter>
